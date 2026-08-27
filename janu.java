764a4c3b76969c0ddf58ca25f10c8903ef10acd6
janaki2007 <janaki24f81a0550@gkce.edu.in>
2026-08-27 11:23:42 +0530

    Add files via upload

--- janu.java
+++ janu.java
@@ -1,7 +1,7 @@
 class main{
     public static void main(String[] args) {
         {
-            System.out.println("hellow world");
+            System.out.println("@cipherflux-labs");
         }
     }
 }
