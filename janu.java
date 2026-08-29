9afcee3e9fe13af3edd56231b0ef9a4484496242
janaki2007 <janaki24f81a0550@gkce.edu.in>
2026-08-29 16:19:31 +0530

    Add files via upload

--- janu.java
+++ janu.java
@@ -1,7 +1,10 @@
 class main{
     public static void main(String[] args) {
-        {
-            System.out.println("@cipherflux-labs");
-        }
+        
+            int n=4;
+            if(n%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
     }
 }
