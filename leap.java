d6d1c5a78b24cab4d5d6283b4123eef05b9938cf
janaki2007 <janaki24f81a0550@gkce.edu.in>
2026-09-23 03:43:35 -0700

    Update and rename janaki1.java to leap.java

---
+++ leap.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int year=2024;
+        if(year%400==0||(year%4==0&&year%100!=0))
+            System.out.println("leap year");
+        else
+               System.out.println(" not a leap year");
+    }
+}
