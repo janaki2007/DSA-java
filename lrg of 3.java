681c95261cb0f415ce81643bb468b99fddbb90ba
janaki2007 <janaki24f81a0550@gkce.edu.in>
2026-09-23 03:38:34 -0700

    Add files via upload

---
+++ lrg of 3.java
@@ -0,0 +1,19 @@
+class large of 3
+{
+    public static void main(String[] args)
+    {
+        int a = 5, b = 10, c = 3;
+        if (a > b && a > c)
+        {
+            System.out.println("Largest number is: " + a);
+        }
+        else if (b > a && b > c)
+        {
+            System.out.println("Largest number is: " + b);
+        }
+        else
+        {
+            System.out.println("Largest number is: " + c);
+        }
+    }
+}
