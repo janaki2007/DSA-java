684a55d584ee59b62c82fb566a57c82e2d1f335a
janaki2007 <janaki24f81a0550@gkce.edu.in>
2026-09-23 03:56:21 -0700

    Update and rename janaki2.java to cnt dgts.java

---
+++ cnt dgts.java
@@ -0,0 +1,12 @@
+class cnt dgts.java
+  {
+  public static void main(String args[])
+    {
+    int n=1 2 3  4,count==0;
+    while(n!=0);
+    n=n/10;
+    count++;
+  }
+  System.out.println(count);
+}
+}
